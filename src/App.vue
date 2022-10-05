6271b59b98113de3843fb5149052500ba73421fb
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-10-05 16:38:00 +0200

    Add imageGallery component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
   <header>
     <div class="wrapper">
       <HelloWorld msg="ROKKA IMAGE GALLERY" />
     </div>
   </header>
-
-  <main></main>
+  <main>
+    <ImageGallery
+      :images="images"
+    />
+  </main>
 </template>
+<script lang="ts" setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import ImageGallery from "./components/ImageGallery.vue";
 
+const images = [
+  {
+    url: 'https://images.unsplash.com/photo-1664575198263-269a022d6e14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    description: 'dAtA aNaLySt',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1664910059414-f7d75a61f907?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    description: 'Maske',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1664970251461-d027c58f94fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    description: 'strand',
+  },
+  {
+    url: 'https://plus.unsplash.com/premium_photo-1661740645196-86113ed93ddd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
+    description: 'essen',
+  },
+  
+]
+</script>
 <style scoped>
 header {
   line-height: 1.5;
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="w-full">
+        <div class="w-full mb-4 grid grid-cols-4 gap-4">
+            <div
+                class="aspect-image"
+                v-for="image of images"   
+            >
+                <img 
+                    class="object-cover w-full h-full"
+                    :src="image.url"
+                    :alt="image.description"
+                />
+            </div>
+        </div>
+        <div class="w-full flex justify-between">
+            <div>
+                <p><a>Alle Fotos Anzeigen</a></p>
+            </div>
+            <div>
+                <p>powered by <b>ROKKA</b></p>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { defineProps, type PropType } from 'vue';
+
+export interface RokkaImage {
+    url: string;
+    description: string;
+} 
+
+defineProps({
+    images: {
+        type: Object as PropType<RokkaImage[]>,
+        required: true,
+    }
+});
+</script>
