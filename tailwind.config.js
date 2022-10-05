88911ac133dfc45d9f5e7157f1e724183d9ede82
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-10-05 16:26:24 +0200

    add tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      aspectRatio: {
+        image: '14 / 9'
+      }
+    },
+  },
+  plugins: [],
+}
