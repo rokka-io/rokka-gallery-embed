77de698c9e3233dab9110ba54580afef30c3f05d
Leo Roettele <leo.roettele@liip.ch>
2022-10-06 17:24:51 +0200

    Show all images in overlay

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -22,9 +22,19 @@
         </div>
     </div>
   <div :class="galleryVisibilityClass" class="bg-black fixed inset-0 z-10 top-0 left-0 right-0 bottom-0 p-16 opacity-90">
-    <CloseButton @click="galleryIsOpen = false" />
-
-
+    <CloseButton @click="galleryIsOpen = false" class="mb-8" />
+    <div class="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-10 gap-4">
+      <div
+          class="aspect-image"
+          v-for="image of images"
+      >
+        <img
+            class="object-cover w-full h-full"
+            :src="image.url"
+            :alt="image.description"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
