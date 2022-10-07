f7054b6e6692e4e4b8be1ce54d5b26149e4a9a02
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-10-07 09:47:34 +0200

    Add clickable rokka image class.

---
+++ src/components/Image.vue
@@ -0,0 +1,23 @@
+<template>
+    <a @click="emit('click', image)">
+        <div class="aspect-image cursor-pointer">
+            <img
+                class="object-cover w-full h-full"
+                :src="image.url"
+                :alt="image.description"
+            />
+        </div>
+    </a>
+</template>
+<script lang="ts" setup>
+import type RokkaImage from '@/classes/RokkaImage';
+import type { PropType } from 'vue';
+defineProps({
+    image: {
+        type: Object as PropType<RokkaImage>,
+        required: true,
+    }
+})
+
+const emit =  defineEmits(['click']);
+</script>
