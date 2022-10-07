d61bced0141be0df5bbdea3d24129251773557e5
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-10-07 10:28:11 +0200

    Implement new image class, use rokka logo, use cursor pointer for clickables.

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,58 +1,49 @@
 <template>
   <div class="w-full">
     <div class="w-full mb-4 grid grid-cols-4 gap-4">
-      <div
-          class="aspect-image"
-          v-for="image of images.slice(0, 4)"
-      >
-        <img
-            class="object-cover w-full h-full"
-            :src="image.url"
-            :alt="image.description"
+        <Image 
+            v-for="image of images.slice(0, 4)"
+            :image="image"
+            @click="(image) => log(image)"
         />
-      </div>
     </div>
     <div class="w-full flex justify-between">
-      <div @click="galleryIsOpen = true">
-        <p><a>Alle Fotos Anzeigen</a></p>
-      </div>
-      <div>
-        <p>powered by <b>ROKKA</b></p>
-      </div>
+        <a class="cursor-pointer" @click="galleryIsOpen = true">
+            <p>Alle Fotos Anzeigen</p>
+        </a>
+      <a class="cursor-pointer" target="_blank" href="https://rokka.io">
+        <p>powered by <Rokka class="inline ml-1"/></p>
+      </a>
     </div>
   </div>
   <div :class="galleryVisibilityClass"
        class="bg-black fixed inset-0 z-10 top-0 left-0 right-0 bottom-0 p-16 opacity-90">
     <CloseButton @click="galleryIsOpen = false" class="mb-8"/>
     <div class="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-10 gap-4">
-      <div
-          class="aspect-image"
-          v-for="image of images"
-      >
-        <img
-            class="object-cover w-full h-full"
-            :src="image.url"
-            :alt="image.description"
+        <Image 
+            v-for="image of images"
+            :image="image"
+            @click="(image) => log(image)"
         />
-      </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { computed, defineProps, type PropType, ref } from 'vue';
+import type RokkaImage from '@/classes/RokkaImage';
 import CloseButton from './Icons/CloseButton.vue'
+import Image from './Image.vue'
+import Rokka from './Icons/Rokka.vue'
 
-export interface RokkaImage {
-  url: string;
-  description: string;
-}
 
 defineProps({
   images: {
-    type: Object as PropType<RokkaImage[]>,
+    type: Array as PropType<RokkaImage[]>,
     required: true,
   }
 });
+
+const log = (d) => console.log(d);
 
 const galleryIsOpen = ref(false)
 const galleryVisibilityClass = computed(() => galleryIsOpen.value ? '' : 'hidden')
