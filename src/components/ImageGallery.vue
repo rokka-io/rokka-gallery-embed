23aa530e63671fd93223611380bb0de7e7b10870
Leo Roettele <leo.roettele@liip.ch>
2022-10-07 14:27:12 +0200

    create a seperate component for overlay

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -2,7 +2,8 @@
   <div class="w-full">
     <div class="w-full mb-4 grid grid-cols-4 gap-4">
         <Image
-            v-for="image of images.slice(0, 4)"
+            v-for="(image, index) of images.slice(0, 4)"
+            :key="index"
             :image="image"
             @click="(image) => log(image)"
         />
@@ -16,24 +17,23 @@
       </a>
     </div>
   </div>
-  <div :class="galleryVisibilityClass"
-       class="bg-black fixed inset-0 z-10 top-0 left-0 right-0 bottom-0 p-16 bg-opacity-90 overflow-y-auto overscroll-contain">
-    <CloseButton @click="galleryIsOpen = false" class="mb-8"/>
+  <Overlay :class="galleryVisibilityClass" @close-overlay="galleryIsOpen = false">
     <div class="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-10 gap-4">
-        <Image
-            v-for="image of images"
-            :image="image"
-            @click="(image) => log(image)"
-        />
+      <Image
+        v-for="(image, index) of images"
+        :key="index"
+        :image="image"
+        @click="(image) => log(image)"
+      />
     </div>
-  </div>
+  </Overlay>
 </template>
 <script lang="ts" setup>
 import { computed, defineProps, type PropType, ref, watch } from 'vue';
 import type RokkaImage from '@/classes/RokkaImage';
-import CloseButton from './Icons/CloseButton.vue'
 import Image from './Image.vue'
 import Rokka from './Icons/Rokka.vue'
+import Overlay from "@/components/Overlay.vue"
 
 
 defineProps({
---
+++ src/components/Overlay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-black fixed inset-0 z-10 top-0 left-0 right-0 bottom-0 p-16 bg-opacity-90 overflow-y-auto overscroll-contain">
+    <CloseButton @click="$emit('closeOverlay')" class="mb-8" />
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+import type { PropType } from "vue";
+import type RokkaImage from "@/classes/RokkaImage";
+import CloseButton from "@/components/Icons/CloseButton.vue";
+
+defineProps({
+  images: {
+    type: Array as PropType<RokkaImage[]>,
+    required: true,
+  },
+});
+</script>
