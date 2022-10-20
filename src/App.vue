df8070d5ed9007a5c4232b2afea1b9f6898b6055
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-10-20 16:38:33 +0200

    fix stupoiddd.

--- src/App.vue
+++ src/App.vue
@@ -131,8 +131,8 @@ const favouriteImages: Image[] = useRokkaImages(favouriteRokkaImages);
 // Make sure always four teaser images are available
 // Either Fills up teaser images with images that arent favourited
 // Or return the first 4 favourited images
-const fixedLenghtFavouriteImages = favouriteImages.map(img => img.id);
-const teaserImages = favouriteImages.length < 4 ?
+const favouriteImageIds = favouriteImages.map(img => img.id);
+const fixedLenghtFavouriteImages = favouriteImages.length < 4 ?
   favouriteImages.concat(
     images
       .filter(img => !favouriteImageIds.includes(img.id))
