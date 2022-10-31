4025bc14801c42c1cb438813dbe47ae86c6a39a8
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-10-31 15:25:20 +0100

    Take stack out of constants.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,6 @@
 const ROKKA_BASE_URL = (organization: string) =>
   `https://${organization}.rokka.io`;
+const ROKKA_BASE_STACK = '/dynamic/resize-width-600-upscale-false-sharpen-true';
 
 export const ROKKA_ENDPOINTS = {
   album: (name: string, organization: string) =>
@@ -7,8 +8,8 @@ export const ROKKA_ENDPOINTS = {
   albumFavourites: (name: string, organization: string) =>
     `${ROKKA_BASE_URL(organization)}/_albums/${name}/favorites.json`,
   render: (hash: string, name: string, organization: string) =>
-    `${ROKKA_BASE_URL(organization
-      )}/dynamic/upscale-false-sharpen-true/o-o.q-6/${hash}/${name}`,
+    `${ROKKA_BASE_URL(organization)}${ROKKA_BASE_STACK}/${hash}/${name}`,
+  // TODO: No route is available for download yet.
   download: (hash: string, name: string, organization: string) =>
     `${ROKKA_BASE_URL(organization)}/download/${hash}/${name}`,
 };
