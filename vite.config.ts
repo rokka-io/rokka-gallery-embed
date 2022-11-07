37f2ac02df7358fa3a08547ae6aab8643ea16adb
Tobias Bazzan <tobias.bazzan@liip.ch>
2022-11-07 13:41:56 +0100

    Build as library in vite config, new export entry point and component.

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    lib: {
+      entry: resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        './src/export.ts'
+      ),
+      name: "RokkaGallery",
+      formats: ['umd'],
+      fileName: "rokka-gallery.js",
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 });
