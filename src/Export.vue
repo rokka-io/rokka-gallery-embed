1058cf461ae17760693eb6b801be063042a06cd4
Tobias Bazzan <35808410+eifachnurtobi@users.noreply.github.com>
2022-12-07 08:47:46 +0100

    Linter.

--- src/Export.vue
+++ src/Export.vue
@@ -11,10 +11,8 @@
 <script setup lang="ts">
 import '@/scss/base.scss';
 import Gallery from './components/Gallery.vue';
-import { onBeforeMount } from 'vue';
-import { useI18n } from 'vue-i18n';
 
-const props = defineProps({
+defineProps({
   organization: {
     type: String,
     required: true,
--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -1,16 +1,17 @@
+import { createI18n } from 'vue-i18n';
 import en from '@/lang/en.json';
 import de from '@/lang/de.json';
-import { createI18n } from 'vue-i18n';
 
 // Export function not object, so that every
 // instance of app has its own instance of i18n
-export default (lang: string) => createI18n({
-  locale: lang,
-  fallbackLocale: 'en',
-  legacy: false,
-  inheritLocale: false,
-  messages: {
-    en: en,
-    de: de,
-  },
-});
+export default (lang: string) =>
+  createI18n({
+    locale: lang,
+    fallbackLocale: 'en',
+    legacy: false,
+    inheritLocale: false,
+    messages: {
+      en: en,
+      de: de,
+    },
+  });
